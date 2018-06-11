14eeabb5018db0102793850fc0ce14154d5bb7d4
rileymd88 <riley.macdonald@gmail.com>
2018-06-11 11:54:07 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "YOUR_API_KEY",
+	authDomain: "YOUR_PROJECT_NAME.firebaseapp.com",
+	databaseURL: "https://YOUR_PROJECT_NAME.firebaseio.com",
+	projectId: "YOUR_PROJECT_NAME",
+	storageBucket: "YOUR_PROJECT_NAME.appspot.com",
+	messagingSenderId: "YOUR_MESSAGING_SENDERID"
+};
